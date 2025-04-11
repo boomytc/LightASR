eb751b1ef77ca6c657a8584f3cc35380ef686971
boomytc <767478228@qq.com>
2025-04-11 13:25:19 +0800

    删除demo_timestamp.py文件中的版权信息和编码声明，简化代码结构

--- paraformer-zh-spk/demo_timestamp.py
+++ paraformer-zh-spk/demo_timestamp.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
-# Copyright FunASR (https://github.com/alibaba-damo-academy/FunASR). All Rights Reserved.
-#  MIT License  (https://opensource.org/licenses/MIT)
-
 from funasr import AutoModel
 import os
 import datetime
